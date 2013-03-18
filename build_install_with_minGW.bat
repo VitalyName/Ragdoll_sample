58d0075383315f86749a7696b488561d3478426f
vredskiy <vitek415@mail.ru>
2013-03-18 21:29:11 +0400

    Modified CMakeLists.txt (fix conflict between debug and release building)

--- build_install_with_minGW.bat
+++ build_install_with_minGW.bat
@@ -4,8 +4,8 @@ SET MinGW_dir="c:\MinGW\bin"
 SET CMake_exe="C:\Program Files (x86)\CMake 2.8\bin\cmake.exe"
 
 ::set dirs
-SET Project_sources_dir="c:\Projects\git_repo\Ragdoll_Sample_test"
-SET Project_builded_dir="c:\Projects\git_repo\Ragdoll_Sample_test_build"
+SET Project_sources_dir="c:\Projects\git_repo\Ragdoll_Sample"
+SET Project_builded_dir="c:\Projects\git_repo\Ragdoll_Sample_build"
 
 
 echo =======================================================
@@ -16,6 +16,10 @@ MKDIR %Project_builded_dir%
 CD %Project_builded_dir%
 
 %CMake_exe% -G "MinGW Makefiles" %Project_sources_dir%
+
+%CMake_exe% -D CMAKE_BUILD_TYPE:STRING=Release %Project_sources_dir%
+mingw32-make.exe
+mingw32-make.exe install
 
 %CMake_exe% -D CMAKE_BUILD_TYPE:STRING=Debug %Project_sources_dir%
 mingw32-make.exe
